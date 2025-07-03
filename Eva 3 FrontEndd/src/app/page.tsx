934ef317599594b94647443e988f84ef68aecd3e
Pxxncho <escorpionxdxt@gmail.com>
2025-07-02 23:54:30 -0400

    ahora si agregamos un formulario basecon los requerimientos pedidos (nombre, edad, etc), pero de momento solo hicimos la estructura base

--- Eva 3 FrontEndd/src/app/page.tsx
+++ Eva 3 FrontEndd/src/app/page.tsx
@@ -6,8 +6,28 @@ export default function Home() {
   return (
     <div>
       <h1>Gestión de Personas</h1>
+
+    <form>
+        <input type="text" name="nombre" placeholder="Nombre" />
+        <input type="number" name="edad" placeholder="Edad" />
+        <select name="tipo">
+          <option value="">Selecciona tipo</option>
+          <option value="Evento">Evento</option>
+          <option value="Beneficiario">Beneficiario</option>
+          <option value="Voluntario">Voluntario</option>
+        </select>
+        <textarea name="descripcion" placeholder="Descripción" />
+        <input type="date" name="fecha" />
+        <button type="submit">Agregar</button>
+    </form>
+
+    <div className="listado">
+        <h2>Listado</h2>
+        <p>No hay registros</p>
+        </div>
     </div>
   );
 }
+
 
 
